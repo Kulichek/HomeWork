a14d5c2275de051051911b1927f5b7043bd36d35
kulichek <kulichek@icloud.com>
2023-02-27 17:15:53 +0300

    четные числа

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,8 +7,8 @@ Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 
-while (count <= number)
+while (count < number)
  {
-  Console.WriteLine(count);
-  count = count + 1;
+  Console.WriteLine(count + 1);
+  count += 2;
 }
