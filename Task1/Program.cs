0538b6864dfe62509a2ae231a1d1880fa3c76383
kulichek <kulichek@icloud.com>
2023-02-27 15:03:54 +0300

    большее,меньшее

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,19 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите числo: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("первое число больше");
+}
+else 
+{
+    Console.WriteLine("второе число больше");
+}
+
+
+
+
