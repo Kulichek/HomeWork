45449f00707fc0410bf40a64158057b3507f742a
kulichek <kulichek@icloud.com>
2023-03-03 13:59:54 +0300

    палиндром

---
+++ HomeTask8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+Console.WriteLine ("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int numberA = a / 10000; // первая цифра
+int numberB = (a / 1000) % 10; // вторая
+int numberC = (a / 10) % 10; //четвертая
+int numberD = a % 10; //пятая
+
+if (numberA == numberD && numberB == numberC)
+{
+    Console.WriteLine("Число палиндром");
+}
+else
+{
+    Console.WriteLine("Число не палиндром");
+}
