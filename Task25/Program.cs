857c929f5495ea56916b1a1cb6c356d4ba89f079
kulichek <kulichek@icloud.com>
2023-03-15 16:33:46 +0300

    возвели число А в натуральную степень В

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+
+    Console.Write("Введите число А: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число В: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int result = 1;
+    for(int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    Console.WriteLine("{0} в степени {1} равно {2}", a, b, result);
+
