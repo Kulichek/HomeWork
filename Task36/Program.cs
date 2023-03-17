dc82ecc1c7486afa4f9c80e3a0fe3452d82aec57
kulichek <kulichek@icloud.com>
2023-03-17 15:55:35 +0300

    в заданном одномерном массиве сумма нечетных чисел

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+int[] array = new int [10];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = random.Next(1, 101);
+}
+int sum = 0;
+for (int i = 1; i < array.Length; i+=2) //сумма элементов на нечетных позициях
+{
+    sum += array[i];
+}
+Console.WriteLine("Рандомный массив");
+foreach (int number in array)
+{
+    Console.WriteLine(number + "");
+}
+Console.WriteLine();
+Console.WriteLine("Сумма нечетных чисел: " + sum);
