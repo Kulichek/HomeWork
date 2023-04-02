60fb18954a57fbcc021505a2a149b11c933a0971
kulichek <kulichek@icloud.com>
2023-04-02 17:34:49 +0300

    элементы больше нуля

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -3,17 +3,45 @@
 //0, 7, 8, -2, -2 -> 2
 
 //1, -7, 567, 89, 223-> 3
+int Promt (string message)
+{
+    System.Console.Write("Введите количество чисел: ");
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int [] Inputarray(int length)
+{
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите i + 1");
+    }
+    return array;
+}
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}]) = {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length = Promt("Введите количество элементов >");
+int[] array;
+array = Inputarray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0  -{CountPositiveNumbers(array)}");
 
-Console.Write("Введите количество чисел: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int count = 0;//счетчик чисел больше нуля
-for (int i = 0; i < n; i++)
-{
-    Console.Write("Введите число: ");
-}
-int num = Convert.ToInt32(Console.ReadLine());
-if (num > 0)
-{
-    count++;
-}
-Console.WriteLine("Количество чисел больше 0: ");
+
