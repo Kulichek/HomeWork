4c902c32d4fc76eac5637ff1dcd789027c2251d5
kulichek <kulichek@icloud.com>
2023-04-04 21:26:33 +0300

    построчный массив с индексом

---
+++ Task60/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+//66(0,0,0) 25(0,1,0)
+//34(1,0,0) 41(1,1,0)
+//27(0,0,1) 90(0,1,1)
+//26(1,0,1) 55(1,1,1)
+
+int[,,]array = new int [2, 2, 2];
+int num = 10;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i, j, k] = num;
+            num++;
+            if (num > 99)
+            break;
+            {
+                if (num > 99)
+                break;
+                {
+                    if (num > 99)
+                    break;
+                }
+            }
+        }
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine("Слой" + i + ":");
+
+    for (int j = 0; j <array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            Console.Write("[" + i + "," + j + "," + k + "]:" + array[i, j, k] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
